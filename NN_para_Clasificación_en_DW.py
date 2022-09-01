1cc74f4140cce08f2a7196f9ef81bddb1f5fd856
Victor Hugo Silva <102427408+victorhugosilvablancas@users.noreply.github.com>
2022-09-01 17:09:11 -0500

    Update NN_para_Clasificación_en_DW.py

--- NN_para_Clasificación_en_DW.py
+++ NN_para_Clasificación_en_DW.py
@@ -1,7 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: latin-1 -*-
 
-import pandas as pd
+import numpy as np #arreglos
+import pandas as pd #datasets 
+import matplotlib.pyplot as plt #graficar
+import seaborn as sns #graficar con estadísticas
 
-df = pd.read_csv('contopolimov.csv') 
-print(df.shape)
+conto_data = pd.read_csv('contopolimov.csv', index_col=0, parse_dates=True)
+print(conto_data.head())
+#conto_data['cargo'].plot()
+#conto_data['abono'].plot()
+#conto_data.plot.scatter(x='cargo', y='abono', alpha=0.5)
+conto_data.plot.box()
+
+#ejes = conto_data.plot.area(figsize=(12,4), subplots=True)
+
+figura,ejes = plt.subplots(figsize=(12,4))
+conto_data.plot.area(ax=ejes) 
+ejes.set_ylabel("movimientos")
+plt.show() 
+figura.savefig("contopolimov.png")
+
+
+conto_data['saldo']=conto_data['cargo'] + conto_data['abono']
+print(conto_data.head())
